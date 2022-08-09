9de4c5da18f6f2724c55f15dfe0a7b0776897e09
Dawa <sherpadawa01@outlook.com>
2022-08-10 01:44:23 +0545

    working on textField and learned about controller

--- lib/main.dart
+++ lib/main.dart
@@ -42,10 +42,15 @@ class _MyHomePageState extends State<MyHomePage> {
   
   
 final List<Transaction> transactions=[
-    Transaction(id:'1', title:'Dawa shop', amount:12.1, date:DateTime.now()),
+    Transaction(id:'1', title:'Dawa shop', amount:12.12, date:DateTime.now()),
     Transaction(id:'2', title:"Weekly shop", amount:22.21, date:DateTime.now()),
   ];
   
+  // String? titleInput;
+  // String? amountInput;
+
+  final titleController= TextEditingController();
+  final amountController=TextEditingController();
 
   void _incrementCounter() {
     setState(() {
@@ -69,58 +74,104 @@ final List<Transaction> transactions=[
        
         title: Text('Expense tracker'),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children:<Widget>[
-           Container(
-             width:double.infinity,
-             child: const Card(
-               color:Colors.blue,
-                child: Text("CHART!"),
-              elevation:5,
-          ),
-           ),
-           Column(
-             
-             children:transactions.map((tx){
-               return Card(
-                //  elevation:3,
-                 child:Row(
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children:<Widget>[
+             Container(
+               width:double.infinity,
+               child: const Card(
+                 color:Colors.blue,
+                  child: Text("CHART!"),
+                elevation:5,
+              ),
+             ),
+             Card(
+               child:Container(
+                 margin:EdgeInsets.all(10),
+                 
+                 child: Column(
+                   crossAxisAlignment: CrossAxisAlignment.end,
                    children: [
-                     Container(
-                       padding:EdgeInsets.all(10),
-                       
-                       margin: EdgeInsets.symmetric(vertical:10, horizontal:15),
-                       decoration:BoxDecoration(border: Border.all(color:Colors.purple, width:2)),
-                       child:Text(
-                          '\$' +tx.amount.toString(),
-                          style:TextStyle(
-                          fontWeight:FontWeight.bold,
-                          fontSize:20,
-                          color:Colors.purple,
-                          )
-                       )
+                     TextField(
+                      decoration:InputDecoration(
+                        labelText:'Title'),
+                        controller:titleController,
+                        // onChanged:(val){
+                        //   titleInput=val;
+                        // }
+                      ),
+                     
+                     TextField(
+                       decoration:InputDecoration(
+                         labelText:"Amount",
+                         
+                       ),
+                       controller:amountController,
+                      //  onChanged:(val){
+                      //    amountInput=val;
+                      //  }
                      ),
-                     Column(
-                       crossAxisAlignment:CrossAxisAlignment.start,
-                       children:<Widget>[
-                         Text(tx.title, style:TextStyle(
-                           fontWeight:FontWeight.bold,
-                           fontSize:16,
-                           )),
-                         Text(DateFormat().format(tx.date), style:TextStyle(
-                           color:Colors.grey,
-                           fontSize:13
-                         ))
-                       ]
-                     )
+      
+                     TextButton(
+                       onPressed: (() => {
+                         print(titleController.text)
+                       }),
+                        child: Text("Add Transaction", style:TextStyle(
+                          color:Colors.purple
+                        )),
+                        style:TextButton.styleFrom(primary:Colors.blue,
+                          // padding: EdgeInsets.symmetric(horizontal: 60, vertical: 10),
+                        )
+      
+                        
+                    )
                    ],
                  ),
-               );
-             }).toList(),
-           )
-        ]
+               )
+             ),
+             Column(
+               
+               children:transactions.map((tx){
+                 return Card(
+                  //  elevation:3,
+                   child:Row(
+                     children: [
+                       Container(
+                         padding:EdgeInsets.all(10),
+                         
+                         margin: EdgeInsets.symmetric(vertical:10, horizontal:15),
+                         decoration:BoxDecoration(border: Border.all(color:Colors.purple, width:2)),
+                         child:Text(
+                            '\$' +tx.amount.toString(),
+                            style:TextStyle(
+                            fontWeight:FontWeight.bold,
+                            fontSize:20,
+                            color:Colors.purple,
+                            )
+                         )
+                       ),
+                       Column(
+                         crossAxisAlignment:CrossAxisAlignment.start,
+                         children:<Widget>[
+                           Text(tx.title, style:TextStyle(
+                             fontWeight:FontWeight.bold,
+                             fontSize:16,
+                             )),
+                           Text(DateFormat.yMMMd().format(tx.date), style:TextStyle(
+                             color:Colors.grey,
+                             fontSize:13
+                           ))
+                         ]
+                       )
+                     ],
+                   ),
+                 );
+               }).toList(),
+             )
+          ]
+        ),
       )
       // This trailing comma makes auto-formatting nicer for build methods.
     );
